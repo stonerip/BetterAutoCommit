7c171ae94d6a88cb4d922742280f193722bac31e
'stonerip' <booguyster@gmail.com>
2015-02-02 23:46:44 -0600

    Its a start

---
+++ Better_Auto_Commit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Better_Auto_Commit
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Title = "GitHub Auto Committ";
+			Github("--version");
+				Console.WriteLine ("Done");
+				Console.ReadLine ();
+		}
+
+		public static void Github(string args)
+		{
+			Process process = new Process();
+			process.StartInfo.RedirectStandardOutput = true;
+			process.StartInfo.RedirectStandardError = true;
+			process.StartInfo.UseShellExecute = false;
+			process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+			process.StartInfo.CreateNoWindow = false;
+			//process.StartInfo.FileName = "C:\\Users\\bsherow\\AppData\\Local\\GitHub\\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\\bin\\git.exe";
+			process.StartInfo.FileName = "git";
+			process.StartInfo.Arguments = args;
+			process.Start();
+			string result = process.StandardOutput.ReadToEnd();
+			Console.WriteLine(result);
+		}
+	}
+}
